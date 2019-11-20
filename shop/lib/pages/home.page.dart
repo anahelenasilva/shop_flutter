508103755de87f07fbc20fe1e95317f3bc5732d2
Ana Helena <anahelenarp@hotmail.com>
2019-11-20 12:53:41 -0300

    Home Page

--- shop/lib/pages/home.page.dart
+++ shop/lib/pages/home.page.dart
@@ -14,6 +14,47 @@ class HomePage extends StatelessWidget {
               height: 60,
             ),
             searchBar(),
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 90,
+              child: categoryList(),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Best Selling",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                FlatButton(
+                  child: Text("See All"),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 350,
+              child: productList(),
+            ),
           ],
         ),
       ),
@@ -57,4 +98,117 @@ Widget searchBar() {
       ],
     ),
   );
+}
+
+Widget categoryList() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+      ],
+    ),
+  );
+}
+
+Widget categoryItem() {
+  return Container(
+    width: 70,
+    height: 70,
+    margin: EdgeInsets.all(10),
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      boxShadow: [
+        new BoxShadow(
+          color: Colors.black12,
+          offset: new Offset(1, 1),
+          blurRadius: 5,
+          spreadRadius: 2,
+        ),
+      ],
+      borderRadius: BorderRadius.all(
+        Radius.circular(64),
+      ),
+    ),
+    child: Image.asset(
+      "assets/Icon_Devices.png",
+    ),
+  );
+}
+
+Widget productItem() {
+  return Container(
+    width: 170,
+    margin: EdgeInsets.all(5),
+    padding: EdgeInsets.all(10),
+    color: Colors.black12,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Image.asset(
+          "assets/product-1.png",
+          width: 170,
+          height: 170,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 60,
+          child: Text(
+            "Título do Produto",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "Marca do Produto",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "\$ 200",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF00C569),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget productList() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+        productItem(),
+      ],
+    ),
+  );
+  ;
 }
